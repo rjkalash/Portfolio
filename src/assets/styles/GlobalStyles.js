adadacd24b47c19fc50931a23e441a1cad91766a
Raj Kalash Tiwari <rajkalashtiwari888@gmail.com>
2025-03-02 04:26:56 +0530

    code cleaning

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,13 +1,12 @@
 // src/assets/styles/GlobalStyles.js
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
-  font-family: 'Courier New', sans-serif;
+    font-family: 'Courier New', sans-serif;
     background-color: #121212;
     color: #ffffff;
   }
