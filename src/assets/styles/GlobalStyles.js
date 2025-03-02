9a947430ec30cb22b3159a54229898d741e08f43
Raj Kalash Tiwari <rajkalashtiwari888@gmail.com>
2025-03-02 09:33:47 +0530

    font changes

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,12 +1,12 @@
 // src/assets/styles/GlobalStyles.js
 import { createGlobalStyle } from "styled-components";
-
 const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Courier New', sans-serif;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 250;
     background-color: #121212;
     color: #ffffff;
   }
